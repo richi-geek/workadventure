52a8cec7256ae563b98837aa7825af43900c69ba
Ricardo Cisneros Araujo <ricardo@MacBook-Pro-de-Ricardo.local>
2023-10-30 15:33:10 -0600

    delete ssh keys

--- terraform/secret-santa/main.tf
+++ terraform/secret-santa/main.tf
@@ -30,8 +30,6 @@ module "backend" {
 
   instance_type      = "t2.micro"
   ami                = "ami-05e8e219ac7e82eba"
-  keyname            = "rici-ssh-backend"
-  public_key         = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8U7V6a0/cPtS70bjqcJ7zBVaDjJlWHx0wqpjBJiHQR terraform"
   subnet_id          = module.network.subnet_id
   security_group_ids = [module.network.security_group_id]
 
@@ -43,8 +41,6 @@ module "frontend" {
 
   instance_type      = "t2.micro"
   ami                = "ami-05e8e219ac7e82eba"
-  keyname            = "rici-ssh-frontend"
-  public_key         = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO8U7V6a0/cPtS70bjqcJ7zBVaDjJlWHx0wqpjBJiHQR terraform"
   subnet_id          = module.network.subnet_id
   security_group_ids = [module.network.security_group_id]
 
